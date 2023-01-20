66ef3ddecb55bd525518b27938dbcf68fa184018
Andrew Tsay <atsay714@gmail.com>
2023-01-19 16:00:13 -0800

    Added comments

--- src/main/java/com/assignment1/DonkeyKongGame.java
+++ src/main/java/com/assignment1/DonkeyKongGame.java
@@ -1,21 +1,57 @@
 package com.assignment1;
 
-interface Entity{
+/**
+ * Interface for all entities in the game, such as Mario, Donkey Kong, and obstacles
+ */
+interface Entity {
+    /**
+     * Method for rendering the entity on the screen
+     */
     void render();
+
+    /**
+     * Method for updating the entity's state
+     */
     void update();
 }
 
+/**
+ * Interface for the player, which extends the Entity interface
+ */
 interface Player extends Entity {
+    /**
+     * Method for moving the player
+     */
     void move();
+
+    /**
+     * Method for making the player jump
+     */
     void jump();
 }
 
+/**
+ * Interface for obstacles in the game, which extends the Entities interface
+ */
 interface Obstacle extends Entity {
+    /**
+     * Method for moving the obstacle
+     */
     void move();
 }
 
+/**
+ * Interface for power-ups in the game, which extends the Entities interface
+ */
 interface PowerUp extends Entity {
+    /**
+     * Method for applying the power-up's effect on the player
+     */
     void apply();
+
+    /**
+     * Method for removing the power-up's effect on the player
+     */
     void remove();
 }
 
